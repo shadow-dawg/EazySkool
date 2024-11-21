343f236ca86908bb3ce811a8cae7890e43540056
shadow-dawg <aradhana1.rai@gmail.com>
2024-11-21 18:20:34 +0530

    second commit

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/miniproject');
+mongoose.connect('your-mongodbcompass-connection');
 
 const userSchema = mongoose.Schema({
     username: String,
